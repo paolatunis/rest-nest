6c8d9a2ab02158939d3eb519be78af7bc9521607
paolatunis <paolatunis@gmail.com>
2023-07-03 19:14:06 -0300

    feat: error handling

---
+++ src/User/common/filters/exception-filter-http.filter.ts
@@ -0,0 +1,37 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { AbstractHttpAdapter, HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class ExceptionFilterHttp implements ExceptionFilter {
+  private httpAdapter: AbstractHttpAdapter;
+
+  constructor(adapterHost: HttpAdapterHost) {
+    this.httpAdapter = adapterHost.httpAdapter;
+  }
+
+  catch(exception: Error, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const request = context.getRequest();
+    const response = context.getResponse();
+
+    const { status, body } =
+      exception instanceof HttpException
+        ? { status: exception.getStatus(), body: exception.getResponse() }
+        : {
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+            body: {
+              statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+              timestamp: new Date().toISOString(),
+              path: request.path,
+              message: exception.message,
+            },
+          };
+    this.httpAdapter.reply(response, body, status);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './User/user.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ExceptionFilterHttp } from './User/common/filters/exception-filter-http.filter';
 
 @Module({
   imports: [UserModule],
@@ -12,6 +13,10 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
     {
       provide: APP_INTERCEPTOR, //interceptador que já é dado pelo próprio Nest
       useClass: ClassSerializerInterceptor, //classe que vai representar o interceptador criado
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionFilterHttp,
     },
   ],
 })
